7e8ba6397170666c7160068aa84f8871842d17e8
Georgy <zmatveyuk@bk.ru>
2026-02-20 17:00:05 +0300

    fix(git): fix git problem

---
+++ src/components/about/about.jsx
@@ -0,0 +1,16 @@
+import "./about.css"
+
+export const About = () => {
+	return (
+		<section className="about">
+			<div className="about-text">
+				<h1 className="all-product">
+					Shop All Products
+				</h1>
+				<p className="discover">
+					Discover the best products for your furry friends
+				</p>
+			</div>
+		</section>
+	);
+}
