11ae06726d4bed99eb1e89cf6a1687475ac48c14
Georgy <102869486+xiqudtt@users.noreply.github.com>
2026-02-20 17:02:36 +0300

    Revert "Feature/under header section"

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import './App.css'
-import { Header } from './components/header/header.jsx'
-import { About } from './components/about/about.jsx'
+
 function App() {
   return (
     <>
-      <Header/>
-			<About/>
+      
     </>
   )
 }
